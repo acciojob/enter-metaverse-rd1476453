7c53202a3d2ad59493fae7da35cc0d592030ea36
rd1476453 <156095993+rd1476453@users.noreply.github.com>
2025-12-09 19:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let statusEle = document.getElementById('status');
+let enterBtnEle = document.getElementById('enterBtn');
+
+enterBtnEle.addEventListener('click',enteredMetaverse);
+
+function enteredMetaverse(){
+	statusEle.innerHTML = "<h1>Entered Metaverse</h1>";
+	
+}
